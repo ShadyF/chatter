697aac129913d3bc4d9e62820b3d6ee50605ea6a
shadyf <shadster09@gmail.com>
2016-12-26 19:12:11 +0200

    Fixed chat-box messagefield not being used properly

--- src/components/views/chat-box/chat-box.jsx
+++ src/components/views/chat-box/chat-box.jsx
@@ -10,36 +10,40 @@ function createMessage(message, index) {
     )
 }
 
-const messageField = (
-    <form>
-        <FormGroup>
-            <InputGroup>
-                <FormControl type="text"
-                             value={props.message_field}
-                             placeholder="Enter Message..."
-                             onChange={e => props.onMessageFieldChange(e.target.value)}
-                />
-                <InputGroup.Button>
-                    <Button type="submit"
-                            onClick={e => {
-                                e.preventDefault();
-                                props.onMessageSend();
-                            }}
-                    >
-                        Send
-                    </Button>
-                </InputGroup.Button>
-            </InputGroup>
-        </FormGroup>
-    </form>
-);
+const messageField = (props) => {
+    "use strict";
+    return (
+        <form>
+            <FormGroup>
+                <InputGroup>
+                    <FormControl type="text"
+                                 value={props.messages.message_field}
+                                 placeholder="Enter Message..."
+                                 onChange={e => props.onMessageFieldChange(e.target.value)}
+                    />
+                    <InputGroup.Button>
+                        <Button type="submit"
+                                onClick={e => {
+                                    e.preventDefault();
+                                    props.onMessageSend();
+                                }}
+                        >
+                            Send
+                        </Button>
+                    </InputGroup.Button>
+                </InputGroup>
+            </FormGroup>
+        </form>
+    )
+};
+
 
 export default function ChatBox(props) {
     "use strict";
     return (
-        <Panel footer={messageField}>
+        <Panel footer={messageField(props)}>
             <ol className="message-box">
-                {props.messages.map(createMessage)}
+                {props.messages.displayed_messages.map(createMessage)}
             </ol>
         </Panel>
     )
