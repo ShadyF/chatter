521b85c0db173a29683bfde1ddd90faff78d8f16
shadyf <shadster09@gmail.com>
2016-12-26 18:54:29 +0200

    updated router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,12 @@
 import {Router, Route, browserHistory, IndexRoute}from 'react-router'
 
+import MainLayout from './components/layouts/main-layout/main-layout'
+import ChatBoxContainer from './components/containers/chat-box-container'
+
 export default (
     <Router history={browserHistory}>
-        <Route path="/" component={}/>
+        <Route component={MainLayout}>
+            <Route path="/" component={ChatBoxContainer}/>
+        </Route>
     </Router>
 )
