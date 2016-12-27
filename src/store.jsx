566667cbe538d46c9ef006fc4dc4fce0d3ac4613
shadyf <shadster09@gmail.com>
2016-12-27 14:17:29 +0200

    store is now linked with redux-devtools chrome extension for debugging purposes

--- src/store.jsx
+++ src/store.jsx
@@ -2,7 +2,10 @@ import {createStore} from 'redux'
 import reducers from './reducers'
 import {initListener} from './socket'
 
-const store = createStore(reducers);
+// const store = createStore(reducers);
+
+// For redux-devtools chrome extension
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Initialize socket listener and attach our store to it
 initListener(store);
