eae53b4ee23d566a2a62677a0bd75f48928bd649
shadyf <shadster09@gmail.com>
2016-12-27 13:19:16 +0200

    replaced normal socket with reconnecting-websocket

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,5 +1,5 @@
 import * as messageActions from './actions/message-actions'
-
+import ReconnectingWebsocket from './reconnecting-websocket'
 const host = 'localhost';
 const port = 8000;
 
@@ -7,7 +7,7 @@ let socket = null;
 
 function initListener(store) {
     "use strict";
-    socket = new WebSocket(host + port + '/chat');
+    socket = new ReconnectingWebsocket('ws://' + host + ':' + port + '/chat');
 
     socket.onmessage = message => {
         store.dispatch(messageActions.addMessage(message.text));
