3376e802cb7f3eac9f244a87db9773ced4ab24af
shadyf <shadster09@gmail.com>
2017-01-01 16:17:36 +0200

    switched to hashHistorty in react-router to avoid the need for a server to handle urls

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Router, Route, browserHistory, IndexRoute}from 'react-router'
+import {Router, Route, browserHistory, hashHistory, IndexRoute}from 'react-router'
 
 import {AppContainer, ChatBoxContainer} from './components'
 
 export default (
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={AppContainer}>
             <IndexRoute component={ChatBoxContainer}/>
         </Route>
