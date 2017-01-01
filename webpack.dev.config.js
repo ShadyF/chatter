c61c013c2d0e75b2a95edb7d8acee1143ffc7706
ShadyF <shadster09@gmail.com>
2017-01-01 20:15:50 +0200

    Added react-fa to dependencies

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -36,7 +36,11 @@ module.exports = {
                 'css-loader?modules&importLoaders=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]',
                 'postcss-loader',
                 'sass-loader?outputStyle=expanded&sourceMap']
-            }
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
         ]
     },
 
